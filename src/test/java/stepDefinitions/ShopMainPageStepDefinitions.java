b2ed68cb43ac1d8572a5f65ac51b84405998db5d
Zagars <gatis.zagars@accenture.com>
2021-03-19 18:12:23 +0200

    polished tasks 1,2

--- src/test/java/stepDefinitions/ShopMainPageStepDefinitions.java
+++ src/test/java/stepDefinitions/ShopMainPageStepDefinitions.java
@@ -34,25 +34,14 @@ public class ShopMainPageStepDefinitions {
         assertEquals(driver.getTitle(),title);
     }
 
-    @Then("^I should see shop page phone top$")
-    public void iShouldSeeShopPagePhoneTop() throws Throwable {
-        assertEquals("0123-456-789",
-                driver.findElement(By.xpath("//strong[contains(text(),'0123-456-789')]")).getText());
-    }
-
     @Then("^I should see shop page phone top PO (.*)$")
     public void iShouldSeeShopPagePhoneTopPO(String topPhone) throws Throwable {
-        assertTrue(shopPage.getTopPhone().toString().contains(topPhone));
+        assertEquals(shopPage.getTopPhone().getText(), topPhone);
     }
 
-    @Then("^I should see shop page phone bottom$")
-    public void iShouldSeeShopPagePhoneBottom() throws Throwable {
-        assertEquals("(347) 466-7432",
-                driver.findElement(By.xpath("//span[contains(text(),'(347) 466-7432')]")).getText());
-    }
 
     @Then("^I should see shop page phone bottom PO (.*)$")
     public void iShouldSeeShopPagePhoneBottomPO(String bottomPhone) throws Throwable {
-        assertTrue(shopPage.getBottomPhone().toString().contains(bottomPhone));
+        assertEquals(shopPage.getBottomPhone().getText(),bottomPhone);
     }
 }
